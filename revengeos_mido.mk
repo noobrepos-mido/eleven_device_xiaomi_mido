Introdructor/revengeos

RevengeOS Bringup...
Signed-off-by: smokey18 <kmboplays@gmail.com>

revengeos_mido.mk
@@ -21,28 +21,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common Ancient ROM stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
-# Gapps
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-
-# Bootanimation Resolution
+# Boot Animation res
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Some Extra's
-ANCIENT_OFFICIAL=true
-
-# OTA for Vanilla Variant only
-FORCE_OTA=true
-
-# For Vanilla
-ANCIENT_NOGAPPS=true
+REVENGEOS_BUILDTYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := ancient_mido
+PRODUCT_NAME := revengeos_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi

noobrepos-mido/eleven_device_xiaomi_mido

revengeos_mido.mk
@@ -21,28 +21,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common Ancient ROM stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
-# Gapps
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-
-# Bootanimation Resolution
+# Boot Animation res
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Some Extra's
-ANCIENT_OFFICIAL=true
-
-# OTA for Vanilla Variant only
-FORCE_OTA=true
-
-# For Vanilla
-ANCIENT_NOGAPPS=true
+REVENGEOS_BUILDTYPE := ALPHA
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := ancient_mido
+PRODUCT_NAME := revengeos_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
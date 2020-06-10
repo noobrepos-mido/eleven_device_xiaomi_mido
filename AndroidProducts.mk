389571ba68490f8cf11e49c4c0bc65bc8a468519
smokey18 <kmboplays@gmail.com>
2020-06-10 17:18:36 +0530

    RevengeOS Bringup...
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_mido.mk
+    $(LOCAL_DIR)/revengeos_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_mido-eng \
-    ancient_mido-userdebug \
-    ancient_mido-user
+    revengeos_mido-eng \
+    revengeos_mido-userdebug \
+    revengeos_mido-user
