e06463d919cd378762540dff88c6a18c4d2fd646
smokey18 <kmboplays@gmail.com>
2020-09-30 09:06:23 +0200

    Toxyc Bringup
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_mido.mk
+    $(LOCAL_DIR)/toxyc_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_mido-eng \
-    ancient_mido-userdebug \
-    ancient_mido-user
+    toxyc_mido-eng \
+    toxyc_mido-userdebug \
+    toxyc_mido-user
