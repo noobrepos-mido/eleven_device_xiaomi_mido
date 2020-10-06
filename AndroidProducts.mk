a82393840a1ae41ff2d254ebca7e53f5f732249a
smokey18 <kmboplays@gmail.com>
2020-10-06 12:57:05 +0200

    AOSAP Bringup
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_mido.mk
+    $(LOCAL_DIR)/aosap_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_mido-eng \
-    ancient_mido-userdebug \
-    ancient_mido-user
+    aosap_mido-eng \
+    aosap_mido-userdebug \
+    aosap_mido-user
